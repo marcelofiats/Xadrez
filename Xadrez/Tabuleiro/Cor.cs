c09745bde3e923b597ce813e248b030faa423e41
Marcelo Fiats <marcelo.fiats@gmail.com>
2020-04-14 10:33:41 -0300

    Criando Tabuleiro

---
+++ Xadrez/Tabuleiro/Cor.cs
@@ -0,0 +1,9 @@
+﻿namespace Xadrez.Tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+
+    }
+}
---
+++ Xadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,10 @@
+﻿namespace Xadrez.Tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao{ get; set; }
+        public Cor cor{ get; set; }
+        public int qteMovimento{ get; set; }
+        public Tabuleiro tab{ get; set; }
+    }
+}
