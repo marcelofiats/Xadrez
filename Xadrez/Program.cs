98bd7613d741d3a9df4f0a36709b77139cf494be
Marcelo Fiats <marcelo.fiats@gmail.com>
2020-04-14 10:14:31 -0300

    Criado posição

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Xadrez.Tabuleiro;
 
 namespace Xadrez
 {
@@ -6,7 +7,11 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição " + P);
         }
     }
 }
